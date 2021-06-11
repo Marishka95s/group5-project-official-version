bd0245a65873391eef9daccb9dec12b50c960124
Mariia Kontseva <marishka95@ukr.net>
2021-06-11 15:47:42 +0300

    Improved structure for group

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
+var debounce = require('lodash.debounce');
+import PicturesApiService from './components/apiService.js'
+import eventTpl from '../templates/eventTpl.hbs';
+
+// import pontyfy styles and js
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import {error} from '@pnotify/core/dist/PNotify.js';
+
 import '../sass/main.scss';
