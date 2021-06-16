87a8ee99adffce6c67a64774fe969ec267d17052
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-06-17 01:30:13 +0300

    search & goTop

--- src/js/components/searchInput.js
+++ src/js/components/searchInput.js
@@ -5,7 +5,7 @@ import error from './error';
 const debounce = require('lodash.debounce');
 
 const API = new Api();
-
+API.query.toLowerCase();
 const eventContainer = document.querySelector('.gallery_list');
 const input = document.querySelector('.search-input');
 input.addEventListener('input', debounce(onSearch, 500));
@@ -13,7 +13,7 @@ input.addEventListener('input', debounce(onSearch, 500));
 
 
 function onSearch(event) {
-    API.query = event.target.value.trim();
+    API.query = event.target.value.toLowerCase().trim();
 
     console.log(API.query);
 
